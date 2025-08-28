31c1b4312b80ff097f60db57f7785183de43633d
murchana0314 <gogoimurchana7@gmail.com>
2025-08-28 14:04:56 +0530

    Add files via upload

--- day1.c
+++ day1.c
@@ -1,1 +1,14 @@
-This is day1 of the 100 Days of Code 
+#include<stdio.h>
+
+int main() {
+    //To input two numbers and display their sum
+    int num1 , num2 , sum;
+    printf("enter num1 :");
+    scanf("%d" ,&num1);
+    printf("enter num2 :");
+    scanf("%d" ,&num2);
+    sum = num1 + num2;
+    printf("the sum is : %d\n" ,sum);
+    return 0;
+
+}
